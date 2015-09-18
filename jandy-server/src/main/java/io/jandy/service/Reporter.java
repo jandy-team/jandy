50dc3d515c92085968e325f961f3bf38668d5b1c
JCooky <bak723@gmail.com>
2015-09-18 21:39:29 +0900

    remove unused import

--- jandy-server/src/main/java/io/jandy/service/Reporter.java
+++ jandy-server/src/main/java/io/jandy/service/Reporter.java
@@ -1,34 +1,18 @@
 package io.jandy.service;
 
 import com.google.common.collect.ImmutableMap;
-import com.steadystate.css.parser.CSSOMParser;
-import com.steadystate.css.parser.SACParserCSS3;
 import io.jandy.domain.Build;
 import io.jandy.domain.User;
 import io.jandy.util.SmallTime;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
-import org.w3c.css.sac.InputSource;
-import org.w3c.dom.css.CSSRule;
-import org.w3c.dom.css.CSSRuleList;
-import org.w3c.dom.css.CSSStyleSheet;
 
 import javax.mail.MessagingException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.util.concurrent.Future;
 
 /**
  * @author JCooky

ad209b7399e02cde2514e6b9774ca0ef18c032ce
jcooky <bak723@gmail.com>
2016-03-06 21:26:09 +0900

    Port #33 to v0.4

--- jandy-server/src/main/java/io/jandy/web/IndexController.java
+++ jandy-server/src/main/java/io/jandy/web/IndexController.java
@@ -1,12 +1,8 @@
 package io.jandy.web;
 
-import io.jandy.web.util.UserCookieGenerator;
-import org.apache.commons.lang3.StringUtils;
+import io.jandy.config.social.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author JCooky
@@ -14,10 +10,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class IndexController {
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String index(HttpServletRequest request) {
-    UserCookieGenerator userCookieGenerator = new UserCookieGenerator();
-    if (!StringUtils.isEmpty(userCookieGenerator.readCookieValue(request))) {
+  @RequestMapping(value = "/")
+  public String index() {
+
+    if (SecurityContext.userSignedIn()) {
       return "forward:/repos";
     } else {
       return "index";

d4c1cd2e5eec41ae1690a0b8274ad8448f28ff86
JCooky <bak723@gmail.com>
2016-04-18 08:27:06 +0900

    Modify travis_docker and travis.yml that travis build of PR commit do not deploy docker

---
+++ travis_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+  exit 0
+fi
+
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+  DOCKER_TAG=latest
+else
+  DOCKER_TAG=$TRAVIS_BRANCH
+fi
+
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASSWD"
+docker build -t jcooky/jandy:$DOCKER_TAG .
+docker push jcooky/jandy:$DOCKER_TAG
+
